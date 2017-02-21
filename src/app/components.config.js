785c2377caaf4722f9bb870797eeceb2e7ba11b4
Wenish <jonas.voland@bluewin.ch>
2017-02-21 21:44:16 +0100

    init

---
+++ src/app/components.config.js
@@ -0,0 +1,15 @@
+﻿define({
+    // Pages - knockout components that serve as pages
+    pages: [
+        // [Scaffolded pages will be inserted here. To retain this feature, don't remove this comment.]
+        { name: 'home-page', path: 'pages/home/home' },
+        { name: 'about-page', path: 'pages/about/about' }
+    ],
+    // Components - knockout components intended as parts of pages, a.k.a. components
+    components: [
+        // [Scaffolded components will be inserted here. To retain this feature, don't remove this comment.]
+        { name: 'nav-bar', path: 'components/nav-bar/nav-bar' },
+        { name: 'app', path: 'components/app/app' }
+
+    ]
+});
